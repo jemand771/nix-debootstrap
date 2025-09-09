39a0845707dec22c008f0f064cc6b6ea0395b2ea
Willy Hille <willy.hille@intenta.de>
2025-09-09 13:43:03 +0000

    replace install-time debootstrap with diy implementation
    we only need a tiny subset of debootstrap's features and the final chroot doesn't even need to be bootable
    (people will mount in /proc and friends anyway)
    
    this also lets us shave off the awkward debootstrap-format source pkgs tar generation

--- lib/default.nix
+++ lib/default.nix
@@ -105,18 +105,6 @@ rec {
         cp -L $dir/package.deb $out/$(cat $dir/name.txt)
       done
     '';
-  baseFile =
-    debs: pkgs.writeText "base" (pkgs.lib.concatStringsSep " " (builtins.map (p: p.Package) debs));
-  requiredFile =
-    debs: pkgs.writeText "required" (pkgs.lib.concatStringsSep "\n" (builtins.map (p: p.Package) debs));
-  debpathsFile =
-    debs:
-    pkgs.writeText "debpaths" (
-      pkgs.lib.concatStringsSep "\n" (
-        # TODO list should be in here too
-        (pkgs.lib.map (deb: "${deb.Package} /var/cache/apt/archives/${debFileName deb}") debs) ++ [ "" ]
-      )
-    );
   resolveDeps =
     packages: deps:
     pkgs.lib.splitString "\n" (
@@ -142,47 +130,55 @@ rec {
       )
     );
   priorityDebs = priority: json: builtins.filter (pkg: pkg.Priority == priority) json;
-  debootstrapTar =
-    required: base:
-    let
-      baseFinal = pkgs.lib.subtractLists required base;
-      allFinal = baseFinal ++ required;
-    in
-    pkgs.runCommand "debootstrap.tar" { src = getDebs allFinal; } ''
-      mkdir -p out/{debootstrap,var/{lib/apt/lists,cache/apt}}
-      cp -r $src out/var/cache/apt/archives
-      # TODO multi-list
-      cp ${pkgs.writeText "list" (json2list allFinal)} out/var/lib/apt/lists/deb.debian.org_debian_dists_trixie_main_binary-amd64_Packages
-      cp ${baseFile baseFinal} out/debootstrap/base
-      cp ${requiredFile required} out/debootstrap/required
-      cp ${debpathsFile allFinal} out/debootstrap/debpaths
-      tar cf $out -C out var debootstrap
-    '';
-  debootstrap = pkgs.debootstrap;
-  debootstrapVerbose = debootstrap.overrideAttrs {
-    postPatch = ''
-      echo 'set -x' | cat - debootstrap > tmp
-      mv tmp debootstrap
-      chmod +x debootstrap
-      sed -i 's/exec >>/# exec >>/' debootstrap
-    '';
-  };
-  buildChroot =
-    required: base:
+  createChroot =
+    debs:
     pkgs.vmTools.runInLinuxVM (
       pkgs.runCommand "chroot.tar"
         {
-          # nativeBuildInputs = [ debootstrapVerbose ];
-          nativeBuildInputs = [ debootstrap ];
+          nativeBuildInputs = [ pkgs.dpkg ];
+        }
+        ''
+          export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+          export DEBIAN_FRONTEND=noninteractive
+          mkdir chroot
+
+          for deb in ${debs}/*; do
+            echo extracting $(basename $deb)
+            dpkg-deb --extract $deb chroot
+          done
+
+          echo installing base system
+          mkdir chroot/pkgs
+          cp ${debs}/* chroot/pkgs/
+          chroot chroot bash -c 'dpkg --install --force-depends /pkgs/*'
+          # (pointlessly) run again without --force-depends to make sure everybody is happy
+          chroot chroot bash -c 'dpkg --install /pkgs/*'
+          rm -rf chroot/pkgs
+
+          echo exporting chroot
+          tar cf $out -C chroot .
+        ''
+    );
+  installPackages =
+    chroot: debs:
+    pkgs.vmTools.runInLinuxVM (
+      pkgs.runCommand "chroot.tar"
+        {
+          src = chroot;
           memSize = 1024 * 8;
         }
         ''
-          debootstrap --unpack-tarball ${debootstrapTar required base} --no-check-sig trixie tmp
-          rm tmp/dev/{null,zero,full,random,urandom,tty,console,ptmx}
-          cat <<EOF >> tmp/root/.bashrc
-          export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
-          EOF
-          tar cf $out -C tmp .
+          export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+          mkdir chroot
+          tar xf $src -C chroot
+
+          mkdir chroot/pkgs
+          cp ${debs}/* chroot/pkgs/
+          chroot chroot bash -c 'apt install -y /pkgs/*'
+          rm -rf chroot/pkgs
+
+          tar cf $out -C chroot .
         ''
     );
+  buildChroot = required: base: installPackages (createChroot (getDebs required)) (getDebs base);
 }
