8040e8528bfa49ddb6fb6281bfb54f8c893c9118
Willy Hille <willy.hille@intenta.de>
2025-09-11 12:36:13 +0000

    drop package list sorting
    since nothing uses debootstrap anymore, this is just redundant

--- lib/lists.nix
+++ lib/lists.nix
@@ -37,12 +37,8 @@ rec {
       builtins.map (
         package:
         pkgs.lib.concatStringsSep "\n" (
-          # debootstrap assumes `Package:` is the first line (lol)
-          # otherwise dependency resolution is off by one package entry with disasterous consequences
-          pkgs.lib.sortOn (line: if pkgs.lib.hasPrefix "Package:" line then 0 else 1) (
-            pkgs.lib.mapAttrsToList (name: value: "${name}: ${value}") (
-              pkgs.lib.filterAttrs (name: _: !pkgs.lib.hasPrefix "_" name) package
-            )
+          pkgs.lib.mapAttrsToList (name: value: "${name}: ${value}") (
+            pkgs.lib.filterAttrs (name: _: !pkgs.lib.hasPrefix "_" name) package
           )
         )
       ) json
